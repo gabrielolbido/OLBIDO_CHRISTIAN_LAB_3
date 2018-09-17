d00200421825cdb5f4f0cfdff32c15846af30de4
Gabriel Olbido <2015081593@ust-ics.mygbiz.com>
2018-09-17 22:47:06 +0800

    3rd commit

--- app/src/androidTest/java/christian/olbido/com/OLBIDO_CHRISTIAN_LAB_3/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/christian/olbido/com/OLBIDO_CHRISTIAN_LAB_3/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("christian.olbidol.com.OLBIDO_CHRISTIAN_LAB_3", appContext.getPackageName());
+        assertEquals("christian.olbido.com.OLBIDO_CHRISTIAN_LAB_3", appContext.getPackageName());
     }
 }
