5b495ae2e5e4051823fc4aff2c65755d9ae9b059
Gabriel Olbido <2015081593@ust-ics.mygbiz.com>
2018-09-17 22:30:13 +0800

    !

---
+++ app/v21/src/main/java/christian/olbido/com/OLBIDO_CHRISTIAN_LAB_3/StylesAndMaterialDesign.java
@@ -0,0 +1,14 @@
+package christian.olbido.com.OLBIDO_CHRISTIAN_LAB_3;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import christian.olbido.com.OLBIDO_CHRISTIAN_LAB_3.*;
+
+public class StylesAndMaterialDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_styles_and_material_design);
+    }
+}
